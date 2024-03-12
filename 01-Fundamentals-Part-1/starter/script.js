338c9155b8107a1fff0944258fffd0de3b6a0bdc
prakash <rai530579@gmail.com>
2024-03-12 22:06:44 +0530

    done assignment 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -2,3 +2,13 @@ let name = "prakash";
 console.log(name);
 if (name === "prakash") alert(name);
 console.log(5 + 7);
+
+// Assignment-1
+
+let country = "India";
+let continent = "Uttar Pradesh";
+let population = "1.4 Billion";
+
+console.log(country);
+console.log(continent);
+console.log(population);
