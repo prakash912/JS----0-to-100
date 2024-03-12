3e2fab3435e79eaeb71585934a25b679bc42a4e7
prakash <rai530579@gmail.com>
2024-03-12 22:36:02 +0530

    lean data types in js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -12,3 +12,22 @@ let population = 100;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+let firstName = "prakash";
+console.log(firstName);
+console.log(typeof firstName);
+
+let count = 5;
+console.log(count);
+console.log(typeof count);
+
+let myNamePrakash = true;
+console.log(myNamePrakash);
+console.log(typeof myNamePrakash);
+
+let weather;
+console.log(weather);
+console.log(typeof weather);
+
+// it returns null type as object that is bug
+console.log(typeof null);
