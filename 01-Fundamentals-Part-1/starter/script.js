f1c382568169e77fe88d0fdc242692bc1b2d211c
prakash <rai530579@gmail.com>
2024-03-13 22:41:35 +0530

    19 --> Challange 2 done

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -88,10 +88,33 @@
 // const markHigherBMI = BMIMark > BMIJohn;
 // console.log(markHigherBMI);
 
-let firstNameMine = "prakash";
-let sentence = "my name is" + " " + firstNameMine + " ";
-console.log(sentence);
+// let firstNameMine = "prakash";
+// let sentence = "my name is" + " " + firstNameMine + " ";
+// console.log(sentence);
 
-// use template litrals
-let litSentence = `my name is ${firstNameMine} .`;
-console.log(litSentence);
+// // use template litrals
+// let litSentence = `my name is ${firstNameMine} .`;
+// console.log(litSentence);
+
+//Challange --> 2
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log("Mark's BMI:", BMIMark, "John's BMI:", BMIJohn);
+
+if (BMIMark > BMIJohn) {
+  console.log("Mark's BMI is higher than John's!");
+} else {
+  console.log("John's BMI is higher than Mark's!");
+}
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
