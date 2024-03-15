3c0a0e798426f2cb7817b3ca5679d3c2a29d43db
prakash <rai530579@gmail.com>
2024-03-15 21:36:52 +0530

    learn type conversion and coersion

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -98,23 +98,34 @@
 
 //Challange --> 2
 
-const massMark = 78;
-const heightMark = 1.69;
-const massJohn = 92;
-const heightJohn = 1.95;
+// const massMark = 78;
+// const heightMark = 1.69;
+// const massJohn = 92;
+// const heightJohn = 1.95;
 
-const BMIMark = massMark / (heightMark * heightMark);
-const BMIJohn = massJohn / (heightJohn * heightJohn);
-console.log("Mark's BMI:", BMIMark, "John's BMI:", BMIJohn);
+// const BMIMark = massMark / (heightMark * heightMark);
+// const BMIJohn = massJohn / (heightJohn * heightJohn);
+// console.log("Mark's BMI:", BMIMark, "John's BMI:", BMIJohn);
 
-if (BMIMark > BMIJohn) {
-  console.log("Mark's BMI is higher than John's!");
-} else {
-  console.log("John's BMI is higher than Mark's!");
-}
+// if (BMIMark > BMIJohn) {
+//   console.log("Mark's BMI is higher than John's!");
+// } else {
+//   console.log("John's BMI is higher than Mark's!");
+// }
 
-if (BMIMark > BMIJohn) {
-  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
-} else {
-  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
-}
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+// } else {
+//   console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+// }
+
+// type conversion
+const inputYear = "1997";
+console.log(inputYear + 5);
+console.log(Number(inputYear) + 5);
+console.log(Number("prakash"));
+console.log(typeof Number("prakash"));
+
+// type coersion
+console.log("5" + 5);
+console.log(5 - "2");
