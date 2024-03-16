c2921919f67d6f5c4c870d8a1d674c5f5eeee07b
prakash <rai530579@gmail.com>
2024-03-16 21:53:54 +0530

    34 --> Function Expression

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -16,3 +16,10 @@ function fruitProcesser(apple, oranges) {
   return juice;
 }
 console.log(fruitProcesser(5, 0));
+
+// function expression
+
+const calcAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+console.log(calcAge(1998));
