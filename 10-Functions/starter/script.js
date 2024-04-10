ef14e3aefcccf8e664e17d5a21f9041055f851b4
prakash <rai530579@gmail.com>
2024-04-10 22:28:32 +0530

    149 --> coding challange 2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -185,3 +185,14 @@ f();
 
 z();
 f();
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+console.dir(z);
